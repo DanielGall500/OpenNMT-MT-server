0154170d33bd587a2b03044495d891b8041ca158
Daniel Gallagher <danielgallagher500@gmail.com>
2023-06-23 16:33:45 +0100

    Delete model-hosting directory

--- model-hosting/run_model_hosting.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-IP=127.0.0.1
-PORT=60000
-URL_ROOT=/translator
-CONFIG=model-hoster/config/config.json
-python model-hoster/config/generate.py
-onmt_server --ip $IP --port $PORT --url_root $URL_ROOT --config $CONFIG
